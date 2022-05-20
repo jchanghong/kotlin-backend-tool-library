6719e0f981facc28feea297d226abaa0e74d0043
jiangchanghong <3200601@qq.com>
2022-05-20 20:26:15 +0800

    init

--- mavenSampleProject-kbtool-min/src/main/java/com/jch/jpa/JpaEntity.java
+++ mavenSampleProject-kbtool-min/src/main/java/com/jch/jpa/JpaEntity.java
@@ -1,12 +1,20 @@
 package com.jch.jpa;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.AbstractAuditable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
-public class JpaEntity {
+@Data
+@Accessors(chain = true)
+@Table(name = "t_jpa")
+public class JpaEntity extends AbstractAuditable<String,UUID> {
 	@GeneratedValue
 	@Id
 	UUID uuid;
--- mavenSampleProject-kbtool-min/src/test/java/com/jch/AppMinTest.java
+++ mavenSampleProject-kbtool-min/src/test/java/com/jch/AppMinTest.java
@@ -4,16 +4,20 @@ import com.github.jchanghong.http.HttpHelper;
 import com.jch.jpa.JpaEntity;
 import com.jch.jpa.JpaRepo;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,23 +36,27 @@ class AppMinTest {
 	@Test
 	void testSwaggerui() {
 		okhttp3.Response synForObject = HttpHelper.INSTANCE.getSynForObject("http://127.0.0.1:" + port + "/swagger-ui/index.html");
-		assertTrue(synForObject.code()==200);
+		assertEquals(200, synForObject.code());
+	}
+
+	@BeforeEach
+	@Transactional
+	void setUp() {
+		jpaRepo.save(new JpaEntity().setName("name1"));
+		int size = jpaRepo.findAll().size();
+		System.out.println(size);
+		assertEquals(size,1);
 	}
 
 	@Test
 	void testjdbc() {
-		template.execute("create table t1(id int)");
-		template.execute("insert into t1 (id) values (1)");
-		List<Map<String, Object>> maps = template.queryForList("select * from t1");
-		System.out.println(maps);
-		assertTrue(maps.size() > 0);
-		for (Map<String, Object> map : maps) {
-			log.info(map.values().toString());
-		}
+		List<JpaEntity> query = template.query("select * from t_jpa", new BeanPropertyRowMapper<JpaEntity>());
+		assertEquals(query.size(),1);
 	}
 
 	@Test
 	void testJpa() {
 		List<JpaEntity> all = jpaRepo.findAll();
+		assertEquals(all.size(),1);
 	}
 }
